d950c9c99bbbd8f2cf5a66ddd2d965f79741e030
Yoonmen <sgu101310@naver.com>
2023-01-16 01:38:46 +0900

    keyFn For CRM

--- CRM_keyFn.py
+++ CRM_keyFn.py
@@ -28,8 +28,8 @@ class KeyFn(QObject) :
             driver = webdriver.Chrome(options=option)
 
         driver.implicitly_wait(3)
-
         driver.get("https://sugang.kumoh.ac.kr/html/stud/sugang.html")
+
         time.sleep(0.2)
         try : 
             driver.find_element(By.ID, "Form_로그인.아이디").send_keys(account[0])
@@ -40,6 +40,7 @@ class KeyFn(QObject) :
 
         try : 
             driver.find_element(By.ID, "Form_버튼.pb_확인").click()
+
             time.sleep(0.2)
             alert = driver.switch_to.alert
             print(alert.text)               # Test code / please delete the contents of this line.
@@ -47,22 +48,45 @@ class KeyFn(QObject) :
                 print("[system] 해당 학년의 수강 신청 기간이 아닙니다.")                # Test code / please delete the contents of this line.
             elif "아이디" in alert.text.split() : 
                 print("[system] 아이디 또는 비밀번호를 다시 확인해 주십시오.")              # Test code / please delete the contents of this line.
+
             alert.accept()
         except NoAlertPresentException : 
             print("[system] 로그인을 완료했습니다.")             # Test code / please delete the contents of this line.
+        
 
+        def registration(subject) : 
+            subjectName, subjectCode = subject[0], subject[1]
 
-        for i in subjectData : 
-            driver.find_element(By.ID, "Form_희망수강과목입력.개설교과목코드").send_keys(i)
+            driver.find_element(By.ID, "Form_희망수강과목입력.개설교과목코드").send_keys(subjectCode)
             try : 
                 driver.find_element(By.ID, "Form_버튼.pb1").click()
+
                 time.sleep(0.2)
                 alert = driver.switch_to.alert
-                if "아니므로" in alert.text.split() : print("[system] 교과목 코드를 다시 확인해주십시오.")
-                elif "이중으로" in alert.text.split() : print("[system] 이미 수강신청을 완료한 과목이므로 다음 과목으로 넘어갑니다.")
+                if "아니므로" in alert.text.split() : 
+                    print("[system] 교과목 코드를 다시 확인해주십시오.")                # Test code / please delete the contents of this line.
+                    return False
+                elif "이중으로" in alert.text.split() : 
+                    print("[system] 이미 수강신청을 완료한 과목입니다.")                # Test code / please delete the contents of this line.
+                    return False
+                # (+ 수강신청하려는 과목의 인원이 마감된 경우)                 # Test code / please delete the contents of this line.
+
                 alert.accept()
             except NoAlertPresentException : 
-                print("[system] 다음 과정으로 넘어갑니다.")             # Test code / please delete the contents of this line.
+                print(f"[system] '{subjectName}' 과목의 수강신청이 완료되었습니다.")                 # Test code / please delete the contents of this line.
+                return True
+
+        subjectData = subjectData.items()
+        majorSubjectCnt = len(subjectData)
+        registrationCnt = 0
+        for major, insuranceLi in subjectData : 
+            if not registration(major) : 
+                for insurance in insuranceLi : 
+                    if registration(insurance) : 
+                        break
+            majorSubjectCnt += 1
+            mainUI.completed_txt_lb.setText(f"{registrationCnt} / {majorSubjectCnt}")
+
         time.sleep(100)                 # Test code / please delete the contents of this line.
         driver.quit()
 
