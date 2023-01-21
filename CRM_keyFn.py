1d3c899dcea52f949f52f0e7de6a89a88e00f0c8
Yoonmen <yoonmen03@gmail.com>
2023-01-22 00:16:30 +0900

    keyFn For CRM

---
+++ CRM_keyFn.py
@@ -0,0 +1,30 @@
+from PySide2.QtCore import QObject
+
+
+class KeyFn(QObject) : 
+    def classRegistration_KIT(self, account, subjectData) : 
+        pass                # Test code / please delete the contents of this line.
+
+
+
+    def classRegistration_DNUE(self, account, subjectData) : 
+        pass                # Test code / please delete the contents of this line.
+
+
+
+    def classRegistration_SKKU(self, account, subjectData) : 
+        pass                # Test code / please delete the contents of this line.
+
+
+
+    def classRegistration_OOO(self, account, subjectData) : 
+        pass                # Test code / please delete the contents of this line.
+
+
+
+
+
+if __name__ == "__main__" : 
+    account = (20772077, 20772077)
+    subjectData = {("努力 未来 A BEAUTIFUL STAR", "YK1012-22"): []}
+    result = KeyFn().classRegistration_KIT(account, subjectData)
